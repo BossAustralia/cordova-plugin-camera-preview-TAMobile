2dc6dc02b221cf51ccb0cc4542073daac7745f8b
BossAustralia <52452280+BossAustralia@users.noreply.github.com>
2019-12-02 18:12:05 +0000

    Update CameraPreview.m

--- src/ios/CameraPreview.m
+++ src/ios/CameraPreview.m
@@ -24,7 +24,7 @@
     return;
   }
 
-  if (command.arguments.count > 3) {
+  if (command.arguments.count >= 12) {
     CGFloat x = (CGFloat)[command.arguments[0] floatValue] + self.webView.frame.origin.x;
     CGFloat y = (CGFloat)[command.arguments[1] floatValue] + self.webView.frame.origin.y;
     CGFloat width = (CGFloat)[command.arguments[2] floatValue];
